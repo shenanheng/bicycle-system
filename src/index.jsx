3495de29ccfb118eb8f88c7a512accedcbc3a97d
shenah <251267741@qq.com>
2019-08-07 09:42:52 +0800

    删除整个项目

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@common/layouts/css/index.less';
-
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
